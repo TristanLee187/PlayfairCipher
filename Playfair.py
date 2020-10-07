ade3dec1ddb893999950c00a3a832ba3d2f4b32c
Tristan Lee <tlee10@stuy.edu>
2020-10-06 21:48:52 -0400

    Add method to fix the plaintext; add X in between duplicates, add Z for odd length

--- Playfair.py
+++ Playfair.py
@@ -1,14 +1,17 @@
 import sys
 
 class Playfair():
-    def __init__(self,keytext,pctext):
-        #Initialize key
-        self.val=[]
-        for i in range(0,25,5):
-            self.val.append(keytext[i:i+5])
+    def __init__(self,args):
+        #Set to encryption or decryption mode
+        self.mode=args[1]
 
         #Initialize the plain or ciphertext
-        self.text=pctext
+        self.text=args[2]
+
+        # Initialize key
+        self.val = []
+        for i in range(0, 25, 5):
+            self.val.append(args[3][i:i + 5])
 
     def indexOf(self,char):
         index=[]
@@ -18,9 +21,33 @@ class Playfair():
                 index.append(self.val[i].index(char))
                 return index
 
-    def print(self):
-        print(self.val)
-        #print(self.indexOf('M'))
+    def fix(self):
+        ans=''
+        window=[]
+        for char in self.text:
+            if len(window)<2:
+                window+=char
+            if len(window)==2:
+                if window[0]==window[1]:
+                    ans+=window[0]+'X'
+                    window[0]=window[1]
+                    window.pop()
+                else:
+                    ans+=''.join(window)
+                    window.clear()
+        ans+=''.join(window)
+        if len(ans)%2==1:
+            ans+='Z'
+        self.text=ans
 
-p=Playfair(sys.argv[3],sys.argv[2])
+    # def encode(self):
+
+
+    def print(self):
+        print(self.mode)
+        print(self.text)
+        print(self.val)
+
+p=Playfair(sys.argv)
+p.fix()
 p.print()
